476a4c18dead750aa38eaa8b2ebacad46d7b4c2d
Anthony Alabado <anthonyalabado3712@gmail.com>
2023-09-04 10:12:00 +0800

    light and dark fixed

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,17 @@ import {
   deleteToDo,
 } from "./utils/HandleApis";
 import mern from "./data/img";
-import ToggleButton from "./components/ToggleButton";
+import { createContext } from "react";
+import ReactSwitch from "react-switch";
+
+export const ThemeContext = createContext(null);
 
 function App() {
   const [toDo, setToDo] = useState([]);
   const [text, setText] = useState("");
   const [isUpdating, setIsUpdating] = useState(false);
   const [toDoId, setToDoId] = useState("");
+  const [theme, setTheme] = useState("light");
 
   useEffect(() => {
     getAllToDo(setToDo);
@@ -25,51 +29,70 @@ function App() {
     setToDoId(_id);
   };
 
-  return (
-    <div className="flex flex-col gap-4 mt-28 mb-10 bg-zinc-700 bg-opacity-25 p-5">
-      <ToggleButton />
-      <div className="flex flex-col items-center gap-3">
-        <h1 className="font-bold">MERN CRUD</h1>
-        <div className="flex gap-2 p-2">
-          {mern.map((mern) => (
-            <div key={mern.id} className="h-10 w-10">
-              <img src={mern.img} alt="" />
-            </div>
-          ))}
-        </div>
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  };
 
-        <div>
-          <input
-            type="text"
-            placeholder="Add Anything"
-            value={text}
-            onChange={(e) => setText(e.target.value)}
-            className="p-2 w-72 text-gray-700 outline-none"
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div id={theme} className="h-screen  flex justify-center w-screen">
+        <div className="flex flex-col gap-4 mt-20 mb-5 w-[40%] h-auto container  p-5">
+          <ReactSwitch
+            onChange={toggleTheme}
+            checked={theme === "dark"}
+            className="absolute top-3 right-3"
           />
-          <button
-            className="hover:border-gray-800"
-            onClick={
-              isUpdating
-                ? () =>
-                    updateToDo(toDoId, text, setToDo, setText, setIsUpdating)
-                : () => addToDo(text, setText, setToDo)
-            }
-          >
-            {isUpdating ? "Update" : "Add"}
-          </button>
+
+          <div className="flex flex-col items-center gap-3">
+            <h1 className="font-bold">MERN CRUD</h1>
+            <div className="flex gap-2 p-2">
+              {mern.map((mern) => (
+                <div key={mern.id} className="h-10 w-10">
+                  <img src={mern.img} alt="" />
+                </div>
+              ))}
+            </div>
+
+            <div className=" w-[70%] flex gap-3">
+              <input
+                type="text"
+                placeholder="Add Anything"
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+                className="p-2 w-full text-gray-700 "
+              />
+              <button
+                className="hover:border-gray-800"
+                onClick={
+                  isUpdating
+                    ? () =>
+                        updateToDo(
+                          toDoId,
+                          text,
+                          setToDo,
+                          setText,
+                          setIsUpdating
+                        )
+                    : () => addToDo(text, setText, setToDo)
+                }
+              >
+                {isUpdating ? "Update" : "Add"}
+              </button>
+            </div>
+          </div>
+          <div className="flex flex-col gap-3  p-2 rounded todos">
+            {toDo.map((item) => (
+              <ToDo
+                key={item._id}
+                text={item.text}
+                updateMode={() => updateMode(item._id, item.text)}
+                deleteToDo={() => deleteToDo(item._id, setToDo)}
+              />
+            ))}
+          </div>
         </div>
       </div>
-      <div className="flex flex-col gap-3 bg-zinc-900 bg-opacity-70 p-2 rounded">
-        {toDo.map((item) => (
-          <ToDo
-            key={item._id}
-            text={item.text}
-            updateMode={() => updateMode(item._id, item.text)}
-            deleteToDo={() => deleteToDo(item._id, setToDo)}
-          />
-        ))}
-      </div>
-    </div>
+    </ThemeContext.Provider>
   );
 }
 
--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -4,7 +4,7 @@ import { AiFillDelete } from "react-icons/ai";
 
 const ToDo = ({ text, updateMode, deleteToDo }) => {
   return (
-    <div className="bg-zinc-700 text-white flex justify-between p-3 bg-opacity-60 rounded-sm w-[25rem]">
+    <div className="bg-zinc-700 flex justify-between p-3 bg-opacity-60 rounded-sm w-full">
       <div className="flex ">{text}</div>
       <div className="icons flex items-center gap-2">
         <BiEdit className="icon" onClick={updateMode} />
